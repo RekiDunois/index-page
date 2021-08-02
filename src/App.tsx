6f14518643bcababdd11eaf47baf2604a0208e0c
RekiDunois <rekidunois+github@gmail.com>
2021-08-02 14:56:32 +0800

    ✨feat(ui): link style and logo
    remove link underline, add translucent background
    
    change logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
@@ -9,14 +9,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div className='App-Title-Container'>
           <div className='App-Title-Body'>
-            <p className='App-Title'>
+            <p className='App-Title Title-Container'>
               海のような波。
             </p>
           </div>
         </div>
         <div className='App-link-container'>
           <a
-            className="App-link"
+            className="App-link Title-Container"
             href="https://blog.rekidunois.cc"
             target="_blank"
             rel="noopener noreferrer"
@@ -24,7 +24,7 @@ function App() {
             Blog
           </a>
           <a
-            className='App-link'
+            className='App-link Title-Container'
             href='https://github.com/RekiDunois'
             target='_blank'
             rel='noopener noreferrer'>
